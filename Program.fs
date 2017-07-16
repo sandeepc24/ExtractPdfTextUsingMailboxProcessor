15fd3497ad2657ee7396dc606effaea548db136b
Sandeep Chandra <sandeepc24@gmail.com>
2017-07-16 13:42:19 +1200

    Initial commit.

---
+++ Program.fs
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Suave  
+open Suave.Filters  
+open Suave.Operators  
+open Suave.Successful
+open PdfUtils
+
+let app =  
+    choose
+        [ GET >=> choose
+            [ 
+                path "/" >=> OK "Index"
+                pathScan "/api/text/%s" getFileText
+            ]
+          POST >=> choose
+            [ 
+                path "/api/text" >=> extractText
+            ] 
+        ]
+
+let initApp() =
+    createAppDirs()
+    processExistingPdfFiles()
+
+
+[<EntryPoint>]
+let main argv =
+    initApp()
+
+    let newConfig = 
+        { defaultConfig with
+            bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 8085 ]
+        }
+    startWebServer newConfig app
+
+    0
